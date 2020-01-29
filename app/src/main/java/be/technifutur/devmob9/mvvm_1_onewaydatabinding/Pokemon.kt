502d16f63b523fa55f844a8918da8fdfde9fb20e
Didier Delhaye <didier.delhaye@drkdidel.be>
2020-01-29 11:35:41 +0100

    validate name

--- app/src/main/java/be/technifutur/devmob9/mvvm_1_onewaydatabinding/Pokemon.kt
+++ app/src/main/java/be/technifutur/devmob9/mvvm_1_onewaydatabinding/Pokemon.kt
@@ -1,6 +1,29 @@
 package be.technifutur.devmob9.mvvm_1_onewaydatabinding
 
 import android.graphics.drawable.Drawable
+import androidx.databinding.BaseObservable
 
-class Pokemon(val name: String, val description: String, val imageResource: Drawable) {
+class Pokemon(
+            name: String = "",
+              val description: String,
+              val imageResource: Drawable,
+
+              var nameError: String? = null
+): BaseObservable() {
+
+    var name: String = name
+    get() = field
+    set(value) {
+        field = value
+        validateName()
+    }
+
+    fun validateName() {
+        if (name.isEmpty()) {
+            nameError = "Veuillez entrer un nom de Pokémon"
+        } else if (!name.matches(Regex("[a-zA-ZÀ-ÿ]+"))) {
+            nameError = "Veuillez n'entrer que des lettres"
+        }
+        notifyChange()
+    }
 }
